d28d6e9bb9f4cd6fac531d2de321593efe169862
mmeder2016 <mmeder2016@gmail.com>
2017-02-02 18:18:54 -0600

    Moved scratchpad.sql to gitignore

--- db/scratchpad.sql
+++
@@ -1,9 +0,0 @@
-DROP DATABASE `chirpy`;
-CREATE DATABASE `sequelize_chirpy`;
-use sequelize_chirpy
-select * from chirps
-
-CREATE DATABASE blogger;
-use burgers_db
-select * from sequelizeburgers
-show tables
